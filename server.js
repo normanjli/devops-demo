3670ccab0afecd2301447f31ce35a76b78559fc8
Norman Li <normanjli@gmail.com>
2021-11-04 08:50:11 -0600

    server  creation

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+// include and initialize the rollbar library with your access token
+app.use(express.json())
+
+const students = [`Adrian`, `Norman`, `Spencer`]
+app.get(`/`, (req,res)=>{
+  res.send(path.join(__dirname, `public/index.html`))
+})
+const port = process.env.PORT || 5050;
+
+app.listen(port, ()=> {console.log(`Server listening on ${port}`)})
