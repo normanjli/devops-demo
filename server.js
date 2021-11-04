d0543734816d77179a158f1e903e6df58f4a45ea
Norman Li <normanjli@gmail.com>
2021-11-04 08:57:01 -0600

    deleted comment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-// include and initialize the rollbar library with your access token
 app.use(express.json())
 
 const students = [`Adrian`, `Norman`, `Spencer`]
+
 app.get(`/`, (req,res)=>{
   res.send(path.join(__dirname, `/public/index.html`))
 })
